736f9e88d577a58a7c3c2cd0299aebdb0a337de0
Neha <nehagadde22@gmail.com>
2025-07-06 23:22:46 +0530

    week-3

---
+++ week-3/Exercise 2 Implementing Dependency Injection/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,16 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(String bookName) {
+        System.out.println("Adding book: " + bookName);
+        bookRepository.saveBook(bookName);
+    }
+}
